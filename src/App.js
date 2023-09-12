b31e4b8b7b123457be86aa13557ffdc939af508b
leoliyou <liyouleoleo@gmail.com>
2023-09-12 23:52:48 +0100

    new children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Notification from "./Notification";
+import Notification from "./Notification";
 import Popup from "./Popup"
 
 function App() {
 
   return (
     <div className="App">
-      <Popup>this is the container -children</Popup>
+      <Popup>
+        <Notification title="Notice" message="This is second" severity="important" />
+      </Popup>
     </div>
   );
 }
